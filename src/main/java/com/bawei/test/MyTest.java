2c7a6095d6b4f678f4204596069d387798b36694
86153 <86153@DESKTOP-FOPUG24>
2020-01-02 20:31:43 +0800

    提交项目

--- src/main/java/com/bawei/test/MyTest.java
+++ src/main/java/com/bawei/test/MyTest.java
@@ -4,7 +4,7 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello word good good good");
+		System.out.println("hello word good good googfhjkd");
 
 	}
 
